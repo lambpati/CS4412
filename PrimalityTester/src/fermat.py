d2437994746bf610e25cd4b71a81b45096a6317e
lambpati <lambpati@isu.edu>
2021-01-23 19:49:10 -0700

    Wrote basis for fermat.py

--- PrimalityTester/src/fermat.py
+++ PrimalityTester/src/fermat.py
@@ -1,41 +1,67 @@
+import math
 import random
 
 
 def prime_test(N, k):
-	# This is main function, that is connected to the Test button. You don't need to touch it.
-	return fermat(N,k), miller_rabin(N,k)
+    # This is main function, that is connected to the Test button. You don't need to touch it.
+    return fermat(N, k), miller_rabin(N, k)
 
 
 def mod_exp(x, y, N):
-    # You will need to implement this function and change the return value.   
-	return 1
-	
+    # You will need to implement this function and change the return value.
+    if y == 0:
+        return 1
+    z = mod_exp(x, math.floor(y / 2), N)
+    if (y % 2) == 0:
+        return math.pow(z, 2) % N
+    else:
+        return x * (math.pow(z, 2) % N)
+
 
 def fprobability(k):
-    # You will need to implement this function and change the return value.   
-    return 0.0
+    # You will need to implement this function and change the return value.
+    pr = 1 / (math.pow(2, k))
+    return pr
 
 
 def mprobability(k):
     # You will need to implement this function and change the return value.   
-    return 0.0
+    pr = 1 / (math.pow(4,k))
+    return pr
 
 
-def fermat(N,k):
+def fermat(N, k):
+    a = []
     # You will need to implement this function and change the return value, which should be
     # either 'prime' or 'composite'.
-	#
+    #
     # To generate random values for a, you will most likley want to use
     # random.randint(low,hi) which gives a random integer between low and
     #  hi, inclusive.
-	return 'prime'
+    assert (k < N), "k is greater than N!"
+    for i in range(k):
+        a.append = random.randint(1, N - 1)
+    if mod_exp(a, N - 1) == 1 % N:
+        return 'prime'
+    else:
+        return 'composite'
 
 
-def miller_rabin(N,k):
+def miller_rabin(N, k):
+    y = N - 1
+    a = []
     # You will need to implement this function and change the return value, which should be
     # either 'prime' or 'composite'.
-	#
+    #
     # To generate random values for a, you will most likley want to use
     # random.randint(low,hi) which gives a random integer between low and
     #  hi, inclusive.
-	return 'composite'
+    assert (k < N), "k is greater than N!"
+    for i in range(k):
+        a.append = random.randint(1, N - 1)
+    while y % 2 == 0:
+        y //= 2
+    if mod_exp(a, y, N) != 1 or mod_exp(a, y, N) != N-1:
+        return 'composite'
+    else:
+        return 'prime'
